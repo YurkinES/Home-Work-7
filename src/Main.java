31b546456e44952fb3868088e4179c3e7a8d2189
YurkinES <6eJlomop@inbox.ru>
2023-03-08 22:38:13 +0300

    task 1 stage

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Task #1
+        int totalMoney = 0;
+        int monthContribution = 15000;
+        int numberOfMonth = 0;
+        while (totalMoney <= 2_459_000) {
+            numberOfMonth++;
+            totalMoney = totalMoney + totalMoney / 100;
+            totalMoney = totalMoney + monthContribution;
+
+        }
+        System.out.println("Месяц " + numberOfMonth + " сумма накоплений равна " + totalMoney + " рублей");
     }
 }
